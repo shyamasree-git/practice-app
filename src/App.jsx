38f7426548bf552e592787e4c9452d332b221b2c
Shyamasree <shyamasree@nimbussystems.com>
2025-08-08 17:46:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2>Welcome to React Project</h2>
+    </>
+  );
+}
+
+export default App;
